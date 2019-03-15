be63a1747365daa9c80d8a12335ab590c1f7f4e7
Kristin Barr <kristinbarr@gmail.com>
2019-03-15 22:57:42 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,19 +3,19 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(value)
+  value
 end
 
-def greeting
-
+def greeting(val1, val2)
+  
 end
 
 def return_a_value
-
+  
 end
 
 def last_evaluated_value
